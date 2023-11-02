fc40095522bbe43d7adae078329afff1c3fc45c8
vperizes <vperizes@gmail.com>
2023-11-01 20:27:39 -0500

    Add functionality to get book title on button click

--- src/index.js
+++ src/index.js
@@ -9,36 +9,50 @@ const books = [
     author: "Matthew Perry",
     img_src: "./images/book1.jpg",
     img_alt: "Mathew Perry's memoir",
-    id: 1,
+    id: 0,
   },
   {
     title: "The Woman in Me",
     author: "Britney Spears ",
     img_src: "https://m.media-amazon.com/images/I/61BWsc9eGbL._SY466_.jpg",
     img_alt: "Britney Spears memoir",
-    id: 2,
+    id: 1,
   },
 ];
 
 const BookList = () => {
+  const getBook = (id) => {
+    //search and find the book whose id matches with the id passed into getBook()
+    const book = books.find((book) => book.id === id);
+    console.log(book);
+  };
+
   return (
     <section className="booklist">
       {books.map((book) => {
-        return <Book book={book} />;
+        return <Book {...book} key={book.id} getBook={getBook} />;
       })}
     </section>
   );
 };
 
 const Book = (props) => {
-  const { author, title, img_src, img_alt, children } = props.book;
+  const { author, title, img_src, img_alt, id, getBook } = props;
+  const displayTitle = () => {
+    console.log(title);
+  };
   return (
     <article className="book">
       <img src={img_src} alt={img_alt} />
       <h2>{title}</h2>
+      <button
+        onClick={() => {
+          getBook(id);
+        }}
+      >
+        display title
+      </button>
       <h4>{author}</h4>
-      {/* where you place children depends on where you want to render it */}
-      {children}
     </article>
   );
 };
